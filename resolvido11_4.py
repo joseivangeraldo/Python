c910f6591492da69e577425a5e2c5b292c058b36
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-10-03 21:44:03 +0000

    2025-10-03 21:44:00 ' Acrescentado'

--- resolvido11_4.py
+++ resolvido11_4.py
@@ -2,18 +2,24 @@
 tem um numero inteiro em cada linha. Todos os numeros lidos devem ser mostrados na tela.
 Mostrar tambem a soma dos valores, a quantidade, a media aritimetica, o menor valor e o maior valor,
 usar um laço while e na leitura usar o metodo.readline()'''
-
 Lst = []
-
-arqEntr = open('entrada_er11_04.txt', 'r')
-linha = arqEntr.readline()
-
+arqEntr = open('entrada_er11_04.txt', 'r')   # abre o arquivo para leitura
+linha = arqEntr.readline()                  # lê a primeira linha
 while linha != '':
-    Lst.append(linha)
-    linha = arqEntr.readline()
-arqEntr.close()
-
+    Lst.append(int(linha))                  # converte linha para inteiro e coloca na lista
+    linha = arqEntr.readline()              # lê a próxima linha
+arqEntr.close()                             # fecha o arquivo
+print('Valores lidos do arquivo')
 print(Lst)
+Soma = sum(Lst)    # calcula a soma dos elementos da lista
+print(f'Soma dos valores = {Soma}')
+Qtde = len(Lst)    # determina a quantidade dos elementos da lista
+print(f'Quantidade = {Qtde}')
+print(f'Média dos valores = {Soma/Qtde}')
+Minimo = min(Lst)  # determina o menor elemento da lista
+print(f'Mínimo dos valores = {Minimo}')
+Maximo = max(Lst)  # determina o maior elemento da lista
+print(f'Máximo dos valores = {Maximo}')
+print('Fim do Programa')
 
-print('\nFim do Programa.')
 
