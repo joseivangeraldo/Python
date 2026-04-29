1885e5047199d70a8c8cc989943c665df6733b0c
joseivangeraldo <ivan.geraldo@gmail.com>
2026-04-29 18:28:32 +0000

    2026-04-29 18:28:29 ' Acrescentado'

---
+++ Udemy-python/aula3.py
@@ -0,0 +1,15 @@
+print(1234)
+
+#aspas simples
+print('Luiz Otavio')
+print(1, '"Luiz Otavio"')
+
+#aspas duplas
+print("Luiz Otavio")
+print(2, "'Luiz Otavio'")
+
+#escape
+print("Luiz \"Otavio\"")
+
+# r
+print(r"Luiz \"Otavio\"")
