23244da1e2331415bc6fe9503b49cc3aebaeb4ef
joseivangeraldo <ivan.geraldo@gmail.com>
2026-04-30 15:17:59 +0000

    2026-04-30 15:17:56 ' Acrescentado'

--- Udemy-python/aula22.py
+++ Udemy-python/aula22.py
@@ -9,15 +9,14 @@
 # Também existe o tipo None que é
 # usado para representar um não valor
 
-# entrada = input('[E]ntrar [S]air: ')
-# senha_digitada = input('Senha: ')
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+senha_permitida = '123456'
 
-# senha_permitida = '123456'
-
-# if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
-#     print('Entrar')
-# else:
-#     print('Sair')
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
 
 # Avaliação de curto circuito
 senha = input('Senha: ') or 'Sem senha'
