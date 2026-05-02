a4ef04cc3232c24c0589131becd8e1c0930dc2ee
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2026-05-02 22:55:32 +0000

    2026-05-02 22:55:29 ' Acrescentado'

---
+++ Udemy-python/61.py
@@ -0,0 +1,33 @@
+"""
+MEU CPF EXEMPLO PRIMEIRO DIGITO:
+10  9   8   7   6   5   4    3    2 
+1   6   2   5   1   7   9    1    8
+10  54  16  35  6   35  36   3   16  += 211
+
+211 * 10 = 2110
+2110 % 11 = 9   
+
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
